59ba7d1ec91c8b2276f95c69ee2dd42e6ea7ee6d
Miron Cuperman <info.deb@nginz.org>
2009-12-06 13:11:27 -0800

    certificate management

--- gem/lib/commands/gitian.rb
+++ gem/lib/commands/gitian.rb
@@ -1,10 +1,15 @@
+require 'net/http'
+require 'net/https'
+require 'digest/sha1'
+require 'rubygems/security'
+
 class Gem::Commands::GitianCommand < Gem::AbstractGitianCommand
   def description
     'Use a Gitian distribution as the primary gem source and enable gem security'
   end
 
   def arguments
-    "[URL]	URL of Gitian distribution, (default #{URL})"
+    "[URL]	URL of Gitian distribution, (see -g for default)"
   end
 
   def usage
@@ -14,13 +19,26 @@ class Gem::Commands::GitianCommand < Gem::AbstractGitianCommand
   def initialize
     super 'gitian', description
 
-    defaults.merge!(:insecure => false, :release => 'latest')
+    defaults.merge!(
+      :insecure => false,
+      :release => 'latest',
+      :gitian => false,
+      :re_get_cert => false
+    )
 
-    add_option('-r', '--release', 'Specify a release (default to latest)') do |value, options|
+    add_option('-r', '--release REL', 'Specify a release (default is "latest")') do |value, options|
       options[:release] = value
     end
 
-    add_option('', '--insecure', 'Do not require signatures on gems (defeats the design goal of Gitian!)') do |value, options|
+    add_option('-g', '--use-gitian', "Switch to #{URL}") do |value, options|
+      options[:gitian] = true
+    end
+
+    add_option('', '--re-get-cert', 'Get the signing certificate again') do |value, options|
+      options[:re_get_cert] = true
+    end
+
+    add_option('', '--insecure', 'Do not require signatures on gems') do |value, options|
       options[:insecure] = true
     end
   end
@@ -40,7 +58,16 @@ class Gem::Commands::GitianCommand < Gem::AbstractGitianCommand
     Gem.configuration["gem"] = gem_opts.strip
     oldurl = Gem.configuration["gitian_source"]
 
-    url = get_one_optional_argument || URL
+    url = get_one_optional_argument
+    if url.nil?
+      if options[:gitian] || oldurl.nil?
+	url = URL
+      else
+	url = URI.parse(oldurl).merge("..").to_s
+      end
+    end
+
+    url += "/" if url[-1,1] != "/"
     url = url + release
     url += "/" if url[-1,1] != "/"
 
@@ -52,9 +79,48 @@ class Gem::Commands::GitianCommand < Gem::AbstractGitianCommand
     Gem.configuration["gitian_source"] = url
 
     Gem.configuration.write
-    say "High security enabled.  You will get an 'unsigned gem' error if you try to install a gem from a normal, non-signing gem repository."
+
+    get_cert(url, options[:re_get_cert])
+
+    if options[:insecure]
+      say "Insecure mode."
+    else
+      say "High security enabled.  You will get an 'unsigned gem' error if you try to install a gem from a normal, non-signing gem repository."
+    end
   end
 
   def show_status
   end
+
+  def get_cert(url, do_force)
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    if uri.scheme == 'https'
+      http.use_ssl = true
+    end
+    http.start do
+      cert_uri = uri.merge("../gem-public_cert.pem")
+      http.request_get(cert_uri.path) do |res|
+	case res
+	when Net::HTTPSuccess
+	  # OK
+	else
+	  $stderr.puts "Could not get certificate at #{cert_uri}"
+	  res.error!
+	end
+	cert = OpenSSL::X509::Certificate.new(res.body)
+	path = Gem::Security::Policy.trusted_cert_path(cert)
+	return if (!do_force && File.exists?(path))
+	Gem::Security.add_trusted_cert(cert)
+	digest = Digest::SHA1.hexdigest(cert.to_der)
+	digest = digest.upcase.gsub(/../, '\0:').chop
+	subject = cert.subject.to_s
+	subject.sub!("/CN=", '')
+	subject.sub!("/DC=", '@')
+	subject.gsub!("/DC=", '.')
+	puts "Please verify fingerprint for <#{subject}> is\n #{digest}"
+      end
+    end
+  end
+
 end
